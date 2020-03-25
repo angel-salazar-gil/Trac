51f63bfc5725a8fff1732ae4f0e9a61688b0bcd8
Edgar <eath2497@gmail.com>
2020-03-25 15:10:37 -0500

    geolocalizacion

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
 declare var google;
 interface Marker {
   position: {
@@ -24,40 +26,46 @@ export class InicioPage implements OnInit {
     },
     {
       position: {
-        lat: 21.1319948,
-        lng: -86.8560091,
-      },
-      title: 'Casa Edgar'
-    },
-    {
-      position: {
         lat: 21.1433426,
         lng: -86.8405372,
       },
       title: 'Parque Urbano Kabah'
     },
   ];
-  constructor() { }
+  constructor(
+    private geolocation: Geolocation
+  ) { }
 
   ngOnInit() {
     this.loadMap();
   }
-  loadMap() {
+  async loadMap() {
+    const rta = await this.geolocation.getCurrentPosition();
     // create a new map by passing HTMLElement
     const mapEle: HTMLElement = document.getElementById('map');
     // create LatLng object
-    const myLatLng = {lat: 21.161669, lng:  -86.851417};
+    const myLatLng = {lat: rta.coords.latitude, lng:  rta.coords.longitude};
     // create map
+    
     this.map = new google.maps.Map(mapEle, {
       center: myLatLng,
-      zoom: 12
+      zoom: 15
     });
-
-    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+    google.maps.event.addListenerOnce(this.map, 'idle', async () => {
       mapEle.classList.add('show-map');
+      const marker = {
+        position: {
+          lat: rta.coords.latitude,
+          lng:  rta.coords.longitude
+        },
+        title: 'Casa Edgar'
+      };
+      this.addMarker(marker);
+
       this.renderMarkers();
     });
   }
+
   renderMarkers() {
     this.markers.forEach(marker => {
       this.addMarker(marker);
