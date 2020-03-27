632008abd6c4ee690ce4d0cf31e5a895917ba7c7
Luis Alberto Diaz de Leon Ochoa <ldiaz1241@gmail.com>
2020-03-27 13:34:56 -0500

    Formato de Reporte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,29 +2,26 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'inicio',
-    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
-  },
-  {
-    path: 'contacto',
-    loadChildren: () => import('./pages/contacto/contacto.module').then( m => m.ContactoPageModule)
-  },
-  {
-    path: '**',
-    redirectTo: 'inicio'
-  },
-  {
-    path: 'reporte',
-    loadChildren: () => import('./pages/reporte/reporte.module').then( m => m.ReportePageModule)
-  },
-
+	{
+		path: 'inicio',
+		loadChildren: () => import('./pages/inicio/inicio.module').then((m) => m.InicioPageModule)
+	},
+	{
+		path: 'contacto',
+		loadChildren: () => import('./pages/contacto/contacto.module').then((m) => m.ContactoPageModule)
+	},
+	{
+		path: 'reportes',
+		loadChildren: () => import('./pages/reporte/reporte.module').then((m) => m.ReportePageModule)
+	},
+	{
+		path: '**',
+		redirectTo: 'inicio'
+	}
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
