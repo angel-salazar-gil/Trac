13b3f674a3e018476b1b5030d2eb65b4fc6d59e3
Edgar <eath2497@gmail.com>
2020-03-31 12:14:55 -0500

    ubicacion en reporte

--- src/app/pages/reporte/reporte.page.ts
+++ src/app/pages/reporte/reporte.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-reporte',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reporte.page.scss'],
 })
 export class ReportePage implements OnInit {
+ubicacion = null;
+constructor(private geolocation: Geolocation) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    const ubicacionInp: HTMLElement = document.getElementById('ubicacion');
+    const rta = await this.geolocation.getCurrentPosition();
+    console.log('la latitude es: ' + rta.coords.latitude + 'la longitud es:' + rta.coords.longitude);
   }
 
 }
